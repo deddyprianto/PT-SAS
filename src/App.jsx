7f104da8d2955c2f0c56d383a66db8a0bf60b6d7
deddyprianto <haicoders12@gmail.com>
2022-02-03 23:13:54 +0700

    add a new dasboard page to PT SAS

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,23 @@
-import React, { useEffect } from 'react';
-import {
-  Routes,
-  Route,
-  useLocation
-} from 'react-router-dom';
-
-import './css/style.scss';
-
-import './charts/ChartjsConfig';
-
+import React, { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import "./css/style.scss";
+import "./charts/ChartjsConfig";
 // Import pages
-import Dashboard from './pages/Dashboard';
+import Dashboard from "./pages/Dashboard";
+import Analisis from "./pages/Analisis";
 
 function App() {
-
   const location = useLocation();
-
   useEffect(() => {
-    document.querySelector('html').style.scrollBehavior = 'auto'
-    window.scroll({ top: 0 })
-    document.querySelector('html').style.scrollBehavior = ''
+    document.querySelector("html").style.scrollBehavior = "auto";
+    window.scroll({ top: 0 });
+    document.querySelector("html").style.scrollBehavior = "";
   }, [location.pathname]); // triggered on route change
 
   return (
     <>
       <Routes>
+        <Route exact path="/analytics" element={<Analisis />} />
         <Route exact path="/" element={<Dashboard />} />
       </Routes>
     </>
